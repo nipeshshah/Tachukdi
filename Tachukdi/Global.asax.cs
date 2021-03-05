d5beaca97c24f78d4f10a171218deeeabd8a33d6
dell\nipesh <nipeshshah9@gmail.com>
2021-03-06 01:36:39 +0530

    sf

--- Tachukdi/Global.asax.cs
+++ Tachukdi/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,7 +12,7 @@ namespace Tachukdi
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        { 
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
